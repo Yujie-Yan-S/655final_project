4d0229e9881681ceb4b8fd710e02cbf05d10b21a
Yujie-Yan-S <cdyyj@bu.edu>
2022-11-26 20:03:16 -0500

    get ip configuration

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require("@vue/cli-service");
+const {defineConfig} = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
+    devServer: {
+        host: "0.0.0.0",
+        public: "192.41.233.62",
+        port: "8080",
+        https: false,
+        disableHostCheck: true,
+        open: false,
+    },
 });
