31b4f2e6651d287c2d387d4a37e7623d375d1ff9
Yujie-Yan-S <113000086+Yujie-Yan-S@users.noreply.github.com>
2022-12-07 15:58:30 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 nvm install lts/fermium
 npm install -g @vue/cli
-git clone https://github.com/Yujie-Yan-S/655final_project.git
